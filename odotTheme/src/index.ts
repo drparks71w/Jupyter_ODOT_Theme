f74287f96939f78c1d3862f313bfcad72b4ad1ac
dane <dane@daneparks.com>
2025-10-15 22:42:13 -0400

    **Working Dark Mode**
    Update ODOT theme: refine UI colors, typography, and layout; switch to a green monochrome palette; enhance CSS structure; update dependencies to latest versions.

--- odotTheme/src/index.ts
+++ odotTheme/src/index.ts
@@ -8,21 +8,20 @@ import { IThemeManager } from '@jupyterlab/apputils';
 /**
  * Initialization data for the odotTheme extension.
  */
-const extension: JupyterFrontEndPlugin<void> = {
-  id: 'odotTheme',
-  requires: [IThemeManager],
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'odotTheme:plugin',
+  description: 'An ODOT branded Jupyter lab theme.',
   autoStart: true,
+  requires: [IThemeManager],
   activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
-    console.log('JupyterLab extension odotTheme is activated!');
     const style = 'odotTheme/index.css';
-
     manager.register({
-      name: 'odotTheme',
-      isLight: true,
+      name: 'ODOT Theme',
+      isLight: false,
       load: () => manager.loadCSS(style),
       unload: () => Promise.resolve(undefined)
     });
   }
 };
 
-export default extension;
+export default plugin;
